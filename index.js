2784af9b04bab6396b634c4c105dff7ba3292067
Bilal Husssain <bilal.mehfooz@soc-solution.com>
2023-10-05 23:51:44 -0700

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-// app.js
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./database/db');
-const domainCheckingRoutes = require('./routes/DomainNameRoute');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const signInWithDomainAndTokenId = require("./sdk/domain-checker-sdk");
+
 const app = express();
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
-// If you're using Express version 4.16+ you can use the built-in middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/auth', domainCheckingRoutes);
-// If you're using an older version of Express or want to use body-parser explicitly
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 
-connectDB();
-
-// ... other code ...
+new signInWithDomainAndTokenId();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`SDK is running on port ${PORT}`);
 });
---
+++ sdk/domain-checker-sdk.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+class signInWithDomainAndTokenId {
+  constructor(apiBaseUrl) {
+    this.apiBaseUrl = apiBaseUrl;
+  }
+
+  async validateUser(username, id) {
+    username = username.toLowerCase();
+    try {
+      const apiEndpoint = "https://mangotreeapi.com/api/findUser";
+
+      const requestBody = {
+        domain: username,
+        id: Number(id),
+      };
+
+      const response = axios.post(apiEndpoint, requestBody);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = signInWithDomainAndTokenId;
